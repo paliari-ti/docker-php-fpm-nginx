9768c9a0d50eff0a01a9ee9d61f0cd76edea09f4
Marcos Paliari <marcos@paliari.com.br>
2019-04-20 15:01:49 -0300

    add SET_PHP_INI_ENV

--- nginx/start.sh
+++ nginx/start.sh
@@ -5,5 +5,12 @@ if [ ! -z "$WEBROOT" ]; then
  sed -i "s#root /var/www/html;#root ${WEBROOT};#g" /etc/nginx/conf.d/default.conf
 fi
 
+# Set php.ini
+if [ ! -z "$SET_PHP_INI_ENV" ]; then
+	if [ -f "/usr/local/etc/php/php.ini-${SET_PHP_INI_ENV}" ]; then
+		cp -f "/usr/local/etc/php/php.ini-${SET_PHP_INI_ENV}" /usr/local/etc/php/php.ini
+    fi
+fi
+
 # Start supervisord and services
 exec /usr/bin/supervisord -n -c /etc/supervisord.conf
